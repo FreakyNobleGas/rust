a55f50b26e78730ee47612b07619e9acf257d2b6
Quinn, Nicholas <Nicholas.Quinn@fmr.com>
2021-12-16 13:01:32 -0500

    Added impl block with methods and associated functions.

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,7 +1,34 @@
+
+// Define Rectangle Struct
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+// Define Methods for Rectangle struct. &self is always first parameter, unless you are
+// defining an associated function, such as a constructor.
+// impl stands for 'implementation' block
+impl Rectangle {
+    // Square Constructor (Note: Since this is associated function, there is no need for self parameter)
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -10,15 +37,38 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 20,
+        height: 30,
+    };
+
+    let sq = Rectangle::square(3);
+
+    println!(
+        "Can Rect2 fit in Rect1? {}",
+        rect1.can_hold(&rect2)
+    );
+
+    println!(
+        "Can Rect2 fit in Sq? {}",
+        sq.can_hold(&rect2)
+    );
+
     // Note area function is borrowing rect1 struct
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
-    dbg!(&rect1);
+    // Derived Debug Statement
+    //dbg!(&rect1);
+
+    if rect1.width() {
+        println!("Rectangle has a non-zero width of {}", rect1.width);
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// No longer needed after defining struct impl block
+//fn area(rectangle: &Rectangle) -> u32 {
+//    rectangle.width * rectangle.height
+//}
